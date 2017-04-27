68d14264f9e7f79733ca931113629f364dfff251
Grant Gapinski <grant-gapinski@bethel.edu>
2017-04-27 18:36:47 -0500

    ex3.py Complete
    Signed-off-by: Grant Gapinski <grant-gapinski@bethel.edu>

---
+++ ex3.py
@@ -0,0 +1,37 @@
+# This line prints a statement
+print "I will now count my chickens:"
+
+#This line prints a statment and then the number of 25 + 5
+print "Hens", 25.0 + 30.0 / 6.0
+#This line prints a statment and then 75 % 4 - 100
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+#PRINTS A STATMENT
+print "Now I will count the eggs:"
+
+#This line goes through the order of opperations
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+#This line prints the statment
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#This line prints the boolean value for whether 5 is less than -2
+print 3.0 + 2.0 < 5.0 - 7.0
+
+#This line prints a statment and then the number
+print "What is 3 + 2?", 3.0 + 2.0
+#This line prints a statment and then the number
+print "What is 5 - 7?", 5.0 - 7.0
+
+#This line prints a statment
+print "Oh, that's why it's False."
+
+#This line prints a statment
+print "How about some more."
+
+#This line prints a statment and then boolean value
+print "Is it greater?", 5.0 > -2.0
+#This line prints a statment and then the boolean value
+print "Is it greater or equal?", 5.0 >= -2.0
+#This line prints a statment and then the boolean value
+print "Is it less or equal?", 5.0 <= -2.0
